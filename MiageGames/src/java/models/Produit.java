92a83f48d3b04837086a396ea530508634db3ebf
pierroclaver <pierroclaver@92d4f9d6-a3e2-05d4-26ec-c7ba64fcb4fd>
2011-04-29 16:11:39 +0000

    ajout des codes dans les entity

--- MiageGames/src/java/models/Produit.java
+++ MiageGames/src/java/models/Produit.java
@@ -4,29 +4,167 @@
  */
 package models;
 
+import java.awt.Image;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 /**
  *
- * @author Sangre
+ * @author Pierro
  */
 @Entity
+@Table(name = "Produit")
+@NamedQueries({
+    @NamedQuery(name = "Produit.findAll", query = "SELECT p FROM Produit p"),
+    @NamedQuery(name = "Produit.findById", query = "SELECT p FROM Produit p WHERE p.id = :id"),
+    @NamedQuery(name = "Produit.findByNom", query = "SELECT p FROM Produit p WHERE p.nom = :nom"),
+    @NamedQuery(name = "Produit.findByPrix", query = "SELECT p FROM Produit p WHERE p.prix = :prix"),
+    @NamedQuery(name = "Produit.findByDistributeur", query = "SELECT p FROM Produit p WHERE p.Distributeur_id = :Distributeur_id"),
+    @NamedQuery(name = "Produit.findByEditeur", query = "SELECT p FROM Produit p WHERE p.editeur = :editeur"),
+    @NamedQuery(name = "Produit.findByDeveloppeur", query = "SELECT p FROM Produit p WHERE p.developpeur = :developpeur"),
+    @NamedQuery(name = "Produit.findByCategorie", query = "SELECT p FROM Produit p WHERE p.Categorie_id = :Categorie_id")
+})
 public class Produit implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Basic(optional = false)
+    @Column(name = "nom")
+    private String nom;
+    
+    @Basic(optional = false)
+    @Column(name = "prix")
+    private BigDecimal prix;
+    
+    // Categorie_id reference la cle primaire "id" de la table "Categorie"
+    @JoinColumn(name = "Categorie_id", referencedColumnName = "id")
+    private Categorie categorie;
+    
+    @JoinColumn(name = "Distributeur_id", referencedColumnName = "id")
+    private Distributeur distributeur;
+    
+    @Column(name = "tags")
+    private ArrayList<String> tags;
+    
+    @Column(name = "developpeur")
+    private String developpeur;
+    
+    @Column(name = "Editeur")
+    private String Editeur;
+    
+    @JoinColumn(name = "CategorieAge_id", referencedColumnName = "id")
+    private CategorieAge categorieAge;
 
-    public Long getId() {
+    public Produit() {
+    }
+
+    public Produit(Integer id) {
+        this.id = id;
+    }
+
+    public Produit(Integer id, String nom, BigDecimal prix, Categorie categorie, Distributeur distributeur, ArrayList<String> tags, String developpeur, String Editeur, CategorieAge categorieAge) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.categorie = categorie;
+        this.distributeur = distributeur;
+        this.tags = tags;
+        this.developpeur = developpeur;
+        this.Editeur = Editeur;
+        this.categorieAge = categorieAge;
+    }
+
+    public String getEditeur() {
+        return Editeur;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public CategorieAge getCategorieAge() {
+        return categorieAge;
+    }
+
+    public String getDeveloppeur() {
+        return developpeur;
+    }
+
+    public Distributeur getDistributeur() {
+        return distributeur;
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public String getNom() {
+        return nom;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setEditeur(String Editeur) {
+        this.Editeur = Editeur;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public void setCategorieAge(CategorieAge categorieAge) {
+        this.categorieAge = categorieAge;
+    }
+
+    public void setDeveloppeur(String developpeur) {
+        this.developpeur = developpeur;
+    }
+
+    public void setDistributeur(Distributeur distributeur) {
+        this.distributeur = distributeur;
+    }
+
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
     }
 
     @Override
@@ -53,5 +191,4 @@ public class Produit implements Serializable {
     public String toString() {
         return "models.Produit[ id=" + id + " ]";
     }
-    
 }
